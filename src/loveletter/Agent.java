ad05ad0fb1645910dab87c092e6ed0ff863835fe
滑板鞋 <aijm73@gmail.com>
2020-11-26 02:31:29 +0800

    Initial commit

---
+++ src/loveletter/Agent.java
@@ -0,0 +1,14 @@
+package loveletter;
+
+/**
+ *  An interface for an agent class <p>
+ *  Contains the following functions: <ol>
+ *  <li> playMove()
+ *  </ol>
+ * 
+ *  @author Jiemin Ai
+ */
+public abstract class Agent {
+    public abstract int playMove();
+    
+}
